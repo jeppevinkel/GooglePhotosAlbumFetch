3711279c5ff12f082f9564a8ba522c19eb5321b6
Jeppe Beier <jeppevinkel@gmail.com>
2024-09-10 21:35:03 +0200

    Add test project

---
+++ GooglePhotosAlbumFetch.Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace GooglePhotosAlbumFetch.Test;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var images = AlbumFetcher.FetchAlbumImages("https://photos.app.goo.gl/1qNqxN6GohxsW1mk8").Result;
+
+        Console.WriteLine(JsonSerializer.Serialize(images));
+    }
+}
