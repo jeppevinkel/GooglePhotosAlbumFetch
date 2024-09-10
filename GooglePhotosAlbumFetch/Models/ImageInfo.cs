34c6561dfd085515f84dfe1df51c923e8f7b3f53
Jeppe Beier <jeppevinkel@gmail.com>
2024-09-10 21:02:20 +0200

    Add documentation

--- GooglePhotosAlbumFetch/Models/ImageInfo.cs
+++ GooglePhotosAlbumFetch/Models/ImageInfo.cs
@@ -2,10 +2,25 @@
 
 public class ImageInfo
 {
-    public string Uid { get; set; } = string.Empty;
-    public List<string> Url { get; set; } = [];
-    public int Width { get; set; }
-    public int Height { get; set; }
-    public long ImageUpdateDate { get; set; }
-    public long AlbumAddDate { get; set; }
+    public string Uid { get; init; } = string.Empty;
+    /// <summary>
+    /// The default URL returns a small version of the image.
+    /// </summary>
+    public string Url { get; init; } = string.Empty;
+    /// <summary>
+    /// A modified URL that returns the full image.
+    /// </summary>
+    public string LargeUrl => $"{Url}=w{Width}-h{Height}";
+    public int Width { get; init; }
+    public int Height { get; init; }
+
+    /// <summary>
+    /// Time the image was updated in Unix Epoch time.
+    /// </summary>
+    public long ImageUpdateDate { get; init; }
+
+    /// <summary>
+    /// Time the image was added to the album in Unix Epoch time.
+    /// </summary>
+    public long AlbumAddDate { get; init; }
 }
---
+++ README.md
@@ -0,0 +1,29 @@
+﻿# GooglePhotosAlbumFetch
+
+A package for fetching permanent photo URLs from shared Google Photo albums.
+
+## Usage
+
+```csharp
+// Note, the url in this example is not to a valid album.
+List<ImageInfo> images = await AlbumFetcher.FetchAlbumImages("https://photos.app.goo.gl/1eGdf543GH4");
+
+foreach(ImageInfo image in images)
+{
+    Console.WriteLine(image.LargeUrl);
+}
+```
+
+## Structure
+
+```json
+{
+    Uid: string
+    Url: string
+    LargeUrl: string
+    Width: int
+    Height: int
+    ImageUpdateDate: long
+    AlbumAddDate: long
+}
+```
